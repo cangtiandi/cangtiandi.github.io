0505bf9809fb689e415f931a03f5ad269b5a3506
Chen <72628138+cangtiandi@users.noreply.github.com>
2021-09-27 11:51:52 -0600

    move from p5.js to vsc

--- Tetris/sketch.js
+++ Tetris/sketch.js
@@ -1,17 +1,40 @@
 // Project Title
+
 // Your Name
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+//global varibles 
+let x = 150;
+let y = 300;
+let drawButton;
+// load the images
+function preload() {
+  logo = loadImage("C:\Users\1302239\Documents\GitHub\cangtiandi.github.io\Pictures\Title");
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  background(255);
+  createCanvas(400, 400);
+  
+  // creating and customizing the button
+  drawButton = createButton("start");
+  drawButton.position(x,y);
+  drawButton.mouseClicked(enterTetris);
+  drawButton.size(width/4,height/8);
+  
+  
+  image(logo, x, 30, 100, 60);
 }
 
 function draw() {
-  background(220);
-  fill("blue");
-  ellipse(mouseX, mouseY, 100 , 50);
+  
+}
+  
+
+function enterTetris(){
+  background(0);
+  drawButton.remove();
 }
