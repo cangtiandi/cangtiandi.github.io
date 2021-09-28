04e84b74abb1993f3ef41c76296d33a7198161f0
Chen <72628138+cangtiandi@users.noreply.github.com>
2021-09-28 12:10:40 -0600

    Update sketch.js

--- Tetris/sketch.js
+++ Tetris/sketch.js
@@ -1,28 +1,28 @@
+/* eslint-disable no-undef */
 // Project Title
-
+// 
 // Your Name
+// Jack Chen
 // Date
-//
+// 9/28/21
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 //global varibles 
 let x = 150;
 let y = 300;
-let drawButton;
 
 // load the images
 function preload() {
   logo = loadImage("assets/Pictures/Title/Tetris.png");
-  blueL = loadImage("assest/Pictures/Blocks/Blue Ricky.png");
-  redZ = loadImage("assest/Pictures/Blocks/Cleveland Z.png");
-  Lblock = loadImage("assest/Pictures/Blocks/Hero.png");
-  orangeL = loadImage("assest/Pictures/Blocks/Orange Ricky.png");
-  greenZ = loadImage("assest/Pictures/Blocks/Rhode Island Z.png");
-  block = loadImage("assest/Pictures/Blocks/Smashboy.png");
-  tBlock = loadImage("assest/Pictures/Blocks/Teewee.png");
+  // blueL = loadImage("assets/Pictures/Blocks/Blue Ricky.png");
+  // redZ = loadImage("assets/Pictures/Blocks/Cleveland Z.png");
+  // lBlock = loadImage("assets/Pictures/Blocks/Hero.png");
+  // orangeL = loadImage("assets/Pictures/Blocks/Orange Ricky.png");
+  // greenZ = loadImage("assets/Pictures/Blocks/Rhode Island Z.png");
+  block = loadImage("assets/Pictures/Blocks/Smashboy.png");
+  // tBlock = loadImage("assets/Pictures/Blocks/Teewee.png");
 }
-
 function setup() {
   background(255);
   createCanvas(400, 400);
@@ -33,12 +33,13 @@ function setup() {
   drawButton.mouseClicked(enterTetris);
   drawButton.size(width/4,height/8);
   
-  
-  image(logo, x, 30, 100, 60);
+  angleMode(DEGREES);
+  imageMode(CENTER);
+
+  image(logo, width/2, 30, 100, 60);
 }
 
 function draw() {
-  image(random());
 }
   
 
@@ -47,6 +48,10 @@ function enterTetris(){
   drawButton.remove();
 }
 
-function game(){
+function mouseClicked(){
 
+}
+
+function keyTyped(){
+  if (key === "a");
 }
