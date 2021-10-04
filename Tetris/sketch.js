bc6bb81bcd8647bc3420d835d83a1397f5f6c86e
Chen <72628138+cangtiandi@users.noreply.github.com>
2021-10-03 21:16:29 -0600

    Update sketch.js

--- Tetris/sketch.js
+++ Tetris/sketch.js
@@ -1,21 +1,21 @@
 /* eslint-disable no-undef */
 // Project Title
-// 
+// Interactive Scene
 // Your Name
-//
+// Jack Chen 
 // Date
 // 9/28/21
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+// Added audio everytime you rotate
 
 //global varibles 
 let x = 150;
 let y = 300;
-let blueL, redZ, lBlock, orangeL, greenZ, block, tBlock;
-let tetrisBlocks = [blueL, redZ, lBlock, orangeL, greenZ, block, tBlock]
-let randomTetrisBlocks = tetrisBlocks[Math.floor(Math.random() * tetrisBlocks.length)]
+let theta = 0;
+let button = false;
 
-// load the images
+// load the images(future use)
 function preload() {
   logo = loadImage("assets/Pictures/Title/Tetris.png");
   blueL = loadImage("assets/Pictures/Blocks/Blue Ricky.png");
@@ -25,7 +25,6 @@ function preload() {
   greenZ = loadImage("assets/Pictures/Blocks/Rhode Island Z.png");
   block = loadImage("assets/Pictures/Blocks/Smashboy.png");
   tBlock = loadImage("assets/Pictures/Blocks/Teewee.png");
-
 }
 
 
@@ -33,29 +32,44 @@ function setup() {
   background(255);
   createCanvas(400, 400);
   
-  // creating and customizing the button
+  // Start menu
   drawButton = createButton("start");
   drawButton.position(x,y);
-  drawButton.mouseClicked(enterTetris);
+  drawButton.mouseClicked(a);
   drawButton.size(width/4,height/8);
+
+  image(logo, width/2, 30, 100, 60);
   
   // adjusting image and angle
   angleMode(DEGREES);
   imageMode(CENTER);
 
-  image(logo, width/2, 30, 100, 60);
+}
+
+function tetrisMode() {
+  background(0);
+  drawButton.remove();
+
+  // rectangle(temporay)
+  translate(200,200)
+  rotate(theta);
+  rectMode(CENTER);
+  rect(0, 0, 50, 100);
 }
 
 function draw() {
- rotation()
-}
-  
-function enterTetris(){
-  background(0);
-  drawButton.remove();
-  image(randomTetrisBlocks, width/2, 30, 100, 60);
+  if (button){
+    tetrisMode();
+  }
 }
 
-function rotation(){
+// rotation
+function keyTyped() {
+  if (key === "d") {
+     theta += 90;
+  }
+}
 
+function a(){
+  button = !button;
 }
