cc9db0de42f4b98da98ad3d7b0048a60528a2ec0
Chen <72628138+cangtiandi@users.noreply.github.com>
2021-10-04 16:02:30 -0600

    added sound every time you rotate the block

--- Tetris/sketch.js
+++ Tetris/sketch.js
@@ -7,24 +7,25 @@
 // 9/28/21
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-// Added audio everytime you rotate
+// Played a sound everyting you rotate the block
 
 //global varibles 
 let x = 150;
 let y = 300;
 let theta = 0;
-let button = false;
+let tetris = false;
+let audio = new Audio("assets/Audio/Block Rotate.mp3")
 
-// load the images(future use)
+// load the images
 function preload() {
   logo = loadImage("assets/Pictures/Title/Tetris.png");
-  blueL = loadImage("assets/Pictures/Blocks/Blue Ricky.png");
-  redZ = loadImage("assets/Pictures/Blocks/Cleveland Z.png");
-  lBlock = loadImage("assets/Pictures/Blocks/Hero.png");
-  orangeL = loadImage("assets/Pictures/Blocks/Orange Ricky.png");
-  greenZ = loadImage("assets/Pictures/Blocks/Rhode Island Z.png");
-  block = loadImage("assets/Pictures/Blocks/Smashboy.png");
-  tBlock = loadImage("assets/Pictures/Blocks/Teewee.png");
+  // blueL = loadImage("assets/Pictures/Blocks/Blue Ricky.png");
+  // redZ = loadImage("assets/Pictures/Blocks/Cleveland Z.png");
+  // lBlock = loadImage("assets/Pictures/Blocks/Hero.png");
+  // orangeL = loadImage("assets/Pictures/Blocks/Orange Ricky.png");
+  // greenZ = loadImage("assets/Pictures/Blocks/Rhode Island Z.png");
+  // block = loadImage("assets/Pictures/Blocks/Smashboy.png");
+  // tBlock = loadImage("assets/Pictures/Blocks/Teewee.png");
 }
 
 
@@ -35,15 +36,26 @@ function setup() {
   // Start menu
   drawButton = createButton("start");
   drawButton.position(x,y);
-  drawButton.mouseClicked(a);
+  drawButton.mouseClicked(enterTetris);
   drawButton.size(width/4,height/8);
 
-  image(logo, width/2, 30, 100, 60);
+  image(logo, x, y/10, 100, 60);
   
   // adjusting image and angle
   angleMode(DEGREES);
   imageMode(CENTER);
 
+}
+
+// activates tetrisMode()
+function enterTetris(){
+  tetris = !tetris;
+}
+
+function draw() {
+  if (tetris){
+    tetrisMode();
+  }
 }
 
 function tetrisMode() {
@@ -57,19 +69,15 @@ function tetrisMode() {
   rect(0, 0, 50, 100);
 }
 
-function draw() {
-  if (button){
-    tetrisMode();
-  }
-}
-
 // rotation
 function keyTyped() {
   if (key === "d") {
      theta += 90;
+     audio.play();
+     }
+  if (key === "a"){
+    theta -= 270;
+    audio.play();
   }
 }
 
-function a(){
-  button = !button;
-}
