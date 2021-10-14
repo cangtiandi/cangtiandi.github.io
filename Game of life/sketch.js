b67207e58135be23b1c76b1044eced8f00404e6c
Chen <72628138+cangtiandi@users.noreply.github.com>
2021-10-14 12:11:04 -0600

    Game of life

---
+++ Game of life/sketch.js
@@ -0,0 +1,21 @@
+// Game of Life
+
+let grid; 
+let gridSize = 10;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  grid = displayGrid(gridSize,gridSize);
+}
+
+function draw() {
+  background(220);
+}
+
+function displayGrid(rows, cols){
+  let x = 0;
+  let y = 0;
+  for (let i=0; i<gridSize; i++) {
+
+  }
+}
