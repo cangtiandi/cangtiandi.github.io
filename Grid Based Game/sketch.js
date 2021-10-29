9db7cb9d56b9025266bc8d0b936e5ed980bc1a99
Chen <72628138+cangtiandi@users.noreply.github.com>
2021-10-29 12:11:46 -0600

    created duck hunt

--- Grid Based Game/sketch.js
+++ Grid Based Game/sketch.js
@@ -1,152 +1,56 @@
-// Project Title
-// Grid Based Game 
-// Your Name
-// Jack Chen 
-// Date
-// 10/19/21
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-// 
 
-//global varibles 
-let theta = 0;
-let tetris = false;
-let logo;
-let drawButton;
-
+// Grid Demo
+let gridSize = 30;
 let grid;
-let gridWidth = 30;
-let gridHeight = 30;
-let cellHeight, cellWidth;
 
-let curTetromino = [];
-let tetrominos = [];
-let tetrominoColour = ["red","blue","green","yellow","purple","orange",];
-let curTetrominoColour;
+let geese;
 
-let startX = 23;
-let startY = 0;
+let level = 0; 
+let score = 0;
 
-class Corrdinates {
-  constructor(x,y) {
-    this.x = x;
-    this.y = y;
-  }
-}
+// class Bird {
+//   constructor(x,cellWidth, cellHeight){
+//     this.x = x;
+//     this.y = 30;
+//     this.dx = random(-5, 5);
+//     this.dy = random(-5, 5);
 
-// load the images
+//   }
+
+//   display() {
+//     imageMode(CENTER);
+//     image(geese, this.x*cellWidth, this.y*cellHeight, cellWidth, cellHeight);
+//   }
+
+//   move() {
+//     let choice = random(75);
+//     if (choice < 25){ //up
+//       this.y -= this.speed;
+//     }
+//     else if (choice < 50){ //left
+//       this.x -= this.speed;
+//     }
+//     else if (choice < 75){ // right
+//       this.x += this.speed;
+//     }
+//   }
+// }
+
 function preload() {
-  logo = loadImage("assets/Pictures/Title/Tetris.png");
+  geese = loadImage("assets/Canadian Geese.png");
 }
-
 
 function setup() {
-  background(255);
   createCanvas(windowWidth, windowHeight);
-  
-  // adjusting image and angle
-  angleMode(DEGREES);
-  imageMode(CENTER);
 
-  // Start menu
-  drawButton = createButton("start");
-  drawButton.position(width/2-100,height/2);
-  drawButton.mouseClicked(enterTetris);
-  drawButton.size(100,100);
-
-  image(logo, width/2-50, height-600, 200, 120);
-
-  // grid 
-  grid = createEmpty2DArray(gridHeight,gridWidth);
-  tetrisBackground();
+  grid = createEmpty2DArray(gridSize,gridSize);
+  theBackground();
 }
 
 function draw() {
-  if (tetris){
-    tetrisMode();
-  }
-}
-
-// activates tetrisMode()
-function enterTetris(){
-  tetris = !tetris;
-}
-
-function tetrisMode() {
-  drawButton.remove();
+  background(220);
   displayGrid();
-  tetrominoBlocks();
-}
-
-function tetrisBackground(){
-  for (let y=0; y<gridHeight; y++){
-    for (let  x=0; x< 12; x++){
-      if(grid[y][x] === 0){
-        grid[y][x] = 2;
-      }
-    }
-  }
-
-  for (let y=0; y<gridHeight; y++){
-    for (let  x=19; x<gridWidth; x++){
-      if(grid[y][x] === 0){
-        grid[y][x] = 2;
-      }
-    }
-  }
-}
-
-
-function tetrominoBlocks() {
-  // Push T 
-  tetrominos.push([[1,0], [0,1], [1,1], [2,1]]);
-  // Push I
-  tetrominos.push([[0,0], [1,0], [2,0], [3,0]]);
-  // Push J
-  tetrominos.push([[0,0], [0,1], [1,1], [2,1]]);
-  // Push Square
-  tetrominos.push([[0,0], [1,0], [0,1], [1,1]]);
-  // Push L
-  tetrominos.push([[2,0], [0,1], [1,1], [2,1]]);
-  // Push S
-  tetrominos.push([[1,0], [2,0], [0,1], [1,1]]);
-  // Push Z
-  tetrominos.push([[0,0], [1,0], [1,1], [2,1]]);
-}
-
-function createTetromino() {
-  let randomTetromino = Math.floor(Math.random() * tetrominos.length);
-  // Create tetris blocks
-  curTetromino = tetrominos[randomTetromino];
-  // Get the colour of it
-  curTetrominoColour = tetrominoColour[randomTetromino];
-}
-
-function drawTetromino() {
-  let x = startX;
-  let y = startY;
-  
-}
-
-function displayGrid(){
-  let cellWidth = width/gridWidth;
-  let cellHeight = height/gridHeight;
-  
-  for (let y=0; y<gridHeight; y++){
-    for (let x=0; x<gridWidth; x++){
-      if (grid[y][x] === 0){
-        fill("white");
-      }
-      if (grid[y][x] === 1){
-        fill("black");
-      }
-      if (grid[y][x] === 2){
-        fill(245,245,220);
-      } 
-      noStroke();
-      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
-    }
-  }
+  geeseMovement();
 }
 
 function createEmpty2DArray(rows,cols){
@@ -160,28 +64,61 @@ function createEmpty2DArray(rows,cols){
   return grid;
 }
 
-// rotation
-function keyTyped() {
-  // eslint-disable-next-line no-undef
-  let switchSound = new Audio("assets/Audio/Block Rotate.mp3");
-  if (key === "d") {
-    theta += 90;
-    switchSound.play();
-  }
-  if (key === "a"){
-    theta -= 270;
-    switchSound.play();
+function displayGrid(){
+  let cellWidth = width/gridSize;
+  let cellHeight = height/gridSize;
+  for (let y=0; y<gridSize; y++){
+    for (let x=0; x<gridSize; x++){
+      if (grid[y][x] === 0){
+        fill("white");
+      }
+      else if (grid[y][x] === 1){
+        fill("black");
+      }
+      else if (grid[y][x] === 2){
+        fill("#ADD8E6"); //light blue
+      }
+      else if (grid[y][x] === 3){
+        fill("#008000");//green
+      }
+      else if (grid[y][x] === 4){
+        fill("#6B4423"); // brown
+        rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+      }
+      else if (grid[y][x] === 5){
+        image(geese, x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+      }
+    }
   }
 }
 
-class Piece {
-  constructor(blocks,x,y){
-    this.blocks = blocks;
-    this.x = x;
-    this.y = y;
-
+function theBackground() {
+  for (let y=2; y< 23; y++){
+    for (let x=0; x<gridSize; x++){
+      if (grid[y][x] === 0){
+        grid[y][x] = 2;
+      }
+    }
   }
 
+  for (let y=23; y<gridSize; y++){
+    for (let x=0; x<gridSize; x++){
+      if (grid[y][x] === 0){
+        grid[y][x] = 3;
+      }
+    }
+  }
+  for (let y=28; y< gridSize; y++){
+    for (let x=0; x<gridSize; x++){
+      if (grid[y][x] === 3){
+        grid[y][x] = 4;
+      }
+    }
+  }
+}
 
-  blocks
+function geeseMovement() {
+  if (grid[20][20] === 2){
+    grid[20][20] = 5;
+  }
 }
